f8eed3d49f0150f7f4a5b864249dfe2a4f6ea4eb
Robert Strutts <Bob_586@Yahoo.com>
2020-04-16 13:49:11 -0400

    Changed photos

--- make_sample.php
+++ make_sample.php
@@ -5,9 +5,12 @@ if (file_exists("sample.png")) {
     exit; 
 }
 
-$string1 = "https://RobertStrutts.com/SocialMediaEncoder/image_decoder.php";
-$string2 = "Upload (this image), to that site";
-$string3 = "to read another message!";
+// Set the enviroment variable for GD
+putenv('GDFONTPATH=' . realpath('.'));
+
+$string1 = "https://RobertStrutts.com/me";
+$string2 = "Save this image & Upload me, to that site";
+$string3 = "to read a secret message!";
 
 $im     = imagecreatefrompng("yoursign.png");
 
@@ -15,8 +18,10 @@ $red   = imagecolorallocate($im, 255, 0, 0);
 $black = imagecolorallocate($im, 0, 0, 0);
 
 $px1     = (imagesx($im) - 7.5 * strlen($string1)) / 2;
-$y1 = 3;
-imagestring($im, 3, $px1, $y1, $string1, $red);
+$y1 = 28;
+$font = "arial.ttf";
+$font_size = 20;
+imagettftext($im, $font_size, 0, $px1, $y1, $red, $font, $string1);
 
 $px2 = 235;
 $y2 = 160;
