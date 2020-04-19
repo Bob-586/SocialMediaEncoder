fba51b2b948433a6cdc833c889bebb6dbc466560
Robert Strutts <Bob_586@Yahoo.com>
2020-04-18 21:58:58 -0400

    Default DB/Table

---
+++ m/sme_db.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 18, 2020 at 09:56 PM
+-- Server version: 8.0.19
+-- PHP Version: 7.3.16-1+ubuntu18.04.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `sme`
+--
+CREATE DATABASE IF NOT EXISTS `sme` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `sme`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` bigint UNSIGNED NOT NULL,
+  `flags` json DEFAULT NULL,
+  `cypher` text NOT NULL,
+  `has_pwd` varchar(1) NOT NULL DEFAULT 'N',
+  `tags` tinytext NOT NULL,
+  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `approved` varchar(1) NOT NULL DEFAULT 'Y'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
